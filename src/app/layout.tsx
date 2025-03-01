3bb45582b8b1d6bb37648ba3de16fc1cafb4bde7
SedationH <sedationh@gmail.com>
2025-03-01 17:38:38 +0800

    add PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="apple-mobile-web-app-title" content="ClipSync" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
